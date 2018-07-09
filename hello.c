84db7337b7cdfd03b73b3ce013be0b2540d7527e
chenke123 <13051695281@163.com>
2018-07-09 13:24:46 +0800

    add file
    add file

---
+++ hello.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2018年07月09日 13时22分26秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world.\n");
+
+    return 0;
+}
